df44e6e48842a45e844ec1b6cbf113cf04b692db
Mudit Jain <muditjdev@gmail.com>
2022-02-04 12:42:08 +0530

    initial commit

---
+++ appshell/src/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome({ user }: { user: string }) {
+    return (
+        <div>
+            Welcome  { user} to the portal !
+        </div>
+    )
+}
+
+export default Welcome;
